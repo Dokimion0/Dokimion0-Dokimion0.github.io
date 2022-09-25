9b380fda8a4ce8eee9f7626957768f26e94440cd
LCY36 <leecy3610@gmail.com>
2022-09-25 17:02:32 +0900

    Style : media query

--- src/components/main/PostItem.tsx
+++ src/components/main/PostItem.tsx
@@ -83,6 +83,12 @@ const ThumbnailImage = styled.img`
   object-fit: cover;
 `
 
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+`
 
 const PostItem: FunctionComponent<PostItemProps> = function ({
   title,
